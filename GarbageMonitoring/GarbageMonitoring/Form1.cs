618d25e7f43fb9ec5bff4302a2d0180747caa873
JitainKalra <38173301+JitainKalra@users.noreply.github.com>
2018-04-08 09:51:22 +0530

    Add files via upload

---
+++ GarbageMonitoring/GarbageMonitoring/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using AForge.Video.DirectShow;
+using AForge.Video;
+
+namespace GarbageMonitoring
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection _fInfoCollection;
+        VideoCaptureDevice _vCaptureDevice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                serialPort1.PortName = Program.com;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+
+            }
+            catch (Exception _exception)
+            {
+                MessageBox.Show("Sorry something went wrong" + _exception);
+            }        
+            
+            
+
+        }
+        int i = 0;
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+           string val=serialPort1.ReadLine();
+           MessageBox.Show(val);
+           string v = val.Substring(0, 1);
+           if (v == "A")
+           {
+               MessageBox.Show("a");
+               string per = val.Substring(2, 2);
+               email_send("Dustbin A has been full " + per + "%");
+
+
+           }
+           if (v == "B")
+           {
+
+               MessageBox.Show("b");
+               string per = val.Substring(2, 2);
+               email_send("Dustbin B has been full " + per + "%");
+
+           }
+            
+           //foreach (string str in value)
+           //{
+           //    MessageBox.Show(str);
+           //}
+          
+        }
+
+        public void email_send(string body)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("Dummydustbin7@gmail.com");
+            mail.To.Add("Dummydustbin7@gmail.com");
+            mail.Subject = "The Dustbin is about to be full";
+            mail.Body = body;
+
+            //System.Net.Mail.Attachment attachment;
+           // attachment = new System.Net.Mail.Attachment("E:\\Img.jpg");
+           // mail.Attachments.Add(attachment);
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("Dummydustbin7@gmail.com", "dummydust7");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _vCaptureDevice = new VideoCaptureDevice(_fInfoCollection[comboBox1.SelectedIndex].MonikerString);
+            _vCaptureDevice.NewFrame += new NewFrameEventHandler(get_Frame);
+            _vCaptureDevice.Start(); 
+        }
+        private void get_Frame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Insert image into Picuture Box
+            Bitmap _BsourceFrame = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = _BsourceFrame;
+        }
+    }
+}
---
+++ SmartDustbin.ino
@@ -0,0 +1,138 @@
+#include <NewPing.h>
+
+#include <NewPing.h>
+
+int trig1=2;
+int echo1=3;
+int trig2=5;
+int echo2=6;
+int maxd=200;
+
+NewPing sonar1(trig1,echo1,maxd);
+NewPing sonar2(trig2,echo2,maxd);
+
+int uS,dist1,dist2;
+
+String s1="LAT:28.5349@LON:77.0558/";
+String s2="LAT:28.6570@LON:77.1115/";
+
+void setup() {
+Serial.begin(9600);
+}
+
+unsigned char flag,flag2=0;
+
+void loop() {
+uS=sonar1.ping();
+dist1=uS/US_ROUNDTRIP_CM;
+
+uS=sonar2.ping();
+dist2=uS/US_ROUNDTRIP_CM;
+
+
+if(dist1==0)
+    dist1=200;
+if(dist2==0)
+    dist2=200;
+
+
+
+if(dist1>=16&&dist1<24 && flag!=1)
+   {
+    
+    delay(3000);
+    uS=sonar1.ping();
+    dist1=uS/US_ROUNDTRIP_CM;
+    if(dist1>=16&&dist1<24 && flag!=1)
+    {
+      Serial.println("A#25!Ais25%");
+      Serial.println(s1); flag=1;  
+       
+    }
+   }
+
+if(dist1>=9&&dist1<16 && flag!=2)
+   {
+    
+    delay(3000);
+    uS=sonar1.ping();
+    dist1=uS/US_ROUNDTRIP_CM;
+    if(dist1>=9&&dist1<16 && flag!=2)
+    {
+      Serial.println("A#50!Ais50%");
+      Serial.println(s1);
+      flag=2;  
+      
+    }
+   }
+
+
+if(dist1>=2&&dist1<9 && flag!=3)
+   { 
+    
+    delay(3000);
+    uS=sonar1.ping();
+    dist1=uS/US_ROUNDTRIP_CM;
+    if(dist1>=2&&dist1<9 && flag!=3)
+    {
+      Serial.println("A#75!Ais75%");
+      Serial.println(s1);
+      flag=3;
+    }
+    
+   }
+
+
+
+
+
+
+
+
+if(dist2>=16&&dist2<24 && flag2!=1)
+   {
+    Serial.println("B#25!Bis25%");
+    delay(3000);
+    uS=sonar2.ping();
+    dist2=uS/US_ROUNDTRIP_CM;
+    if(dist2>=16&&dist2<24 && flag2!=1)
+    {
+      Serial.println("B#25!Bis25%");
+      Serial.println(s2); flag2=1;  
+       
+    }
+   }
+
+if(dist2>=9&&dist2<16 && flag2!=2)
+   {
+    
+    delay(3000);
+    uS=sonar2.ping();
+    dist2=uS/US_ROUNDTRIP_CM;
+    if(dist2>=9&&dist2<16 && flag2!=2)
+    {
+      Serial.println("B#50!Bis50%");
+      Serial.println(s2);
+      flag2=2;  
+      
+    }
+   }
+
+
+if(dist2>=2&&dist2<9 && flag2!=3)
+   { 
+    
+    delay(3000);
+    uS=sonar2.ping();
+    dist2=uS/US_ROUNDTRIP_CM;
+    if(dist2>=2&&dist2<9 && flag2!=3)
+    {
+      Serial.println("B#75!Bis75%");
+      Serial.println(s2);
+      flag2=3;
+    }
+    
+   }
+
+delay(200);
+}
